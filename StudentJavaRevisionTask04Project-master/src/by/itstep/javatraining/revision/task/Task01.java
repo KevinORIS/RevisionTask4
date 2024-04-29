ccebf4180edcaba21ad3f49d88aa4f148a7746f0
User <User@User-PC>
2024-04-29 09:31:47 +0300

    Task 1 solved

--- StudentJavaRevisionTask04Project-master/src/by/itstep/javatraining/revision/task/Task01.java
+++ StudentJavaRevisionTask04Project-master/src/by/itstep/javatraining/revision/task/Task01.java
@@ -40,7 +40,17 @@ package by.itstep.javatraining.revision.task;
  */
 
 public class Task01 {
-    public static int start(int hour1, int minute1, int second1, int hour2, int minute2, int second2) {
-        return -1;
-    }
+	public static int start(int hour1, int minute1, int second1, int hour2, int minute2, int second2) {
+		if (hour1 < 0 || hour1 >= 24 || hour2 < 0 || hour2 >= 24 
+				|| minute1 < 0 || minute1 >= 60 || minute2 < 0 || minute2 >= 60 
+				|| second1 < 0 || second1 >= 60 || second2 < 0 || second2 >= 60) {
+
+			return -1;
+		}
+
+		int totalSeconds1 = hour1 * 3600 + minute1 * 60 + second1;
+		int totalSeconds2 = hour2 * 3600 + minute2 * 60 + second2;
+
+		return Math.abs(totalSeconds1 - totalSeconds2);
+	}
 }
